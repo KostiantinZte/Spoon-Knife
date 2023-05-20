fd6822719a2c30b52c37d8804e21bc0e4d9a1631
KostiantinZte <123990231+KostiantinZte@users.noreply.github.com>
2023-05-20 19:42:39 +0300

    Update Window.h

--- Window.h
+++ Window.h
@@ -1,13 +1,17 @@
 #stream <fsdio.h>
 #define <window.h>
 #define <Target.h>
+#define <ide-3.5.0.jar>
 #define <Target.jar>
 #define <Fopen99.conf>
 #define <EmulatorLibJavac.php>
+#invlude <Java_launcher.dll>
 ****************************************
+$bash sudo apt-get-runtime 
 $bash sudo apt-get-install dogecoin-1.14.6-aarch64-ilinux-gnu-tar.gz;
 $bash sudo apt-get-update Java.dll, d%%;
-$bash sudo apt-get-update Java_library.dll, d%%;
+$bash sudo apt-get-update Java_launcher.dll, d%%;
 $bash sudo apt-get-update Emulator LibJavac.jar, d%%;
+$bash sudo apt-get update EmulatorLibJavac.jar
 $mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
