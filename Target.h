e56195448cdd972e7ce50cb2d319fb6d21531deb
KostiantinZte <123990231+KostiantinZte@users.noreply.github.com>
2023-05-20 19:39:50 +0300

    Add files via upload

---
+++ Target.h
@@ -0,0 +1,395 @@
+#include <Fopen.php>
+#include <Fopen.h>
+#include <Fopen3.conf>
+#include <mmx64.efi>
+#include <mmx32.efi>
+#include <linuz.mod>
+#include <sounds.mod>
+#include <http.mod>
+#include <psi.mod>
+#include <Fopen99.conf>
+#include <Apacheds-2.0.0.deb>
+#include <Target.jar>
+#include <biosdisk.mod>
+#include <usb.mod>
+#include <usbms.mod>
+#include <usb_keyboard.mod>
+#invlude <usbserial_keyboard>
+
+#define <Target1.jar|h>
+//////////////////////////////////////////
+LRESULT CALLBACK (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+LPSTR lpCmdLine, int nCmdShow)
+while (GetMessage(&msg, NULL, 0, 0)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+HMENU hMenuMyContext;
+hMenuMyContext = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU_MYCONTEXT));
+hMenuMyContext = GetSubMenu(hMenuMyContext, 0);
+HWND hwndToolBar = CreateWindow(TOOLBARCLASSNAME, NULL, WS_CHILD | WS_MULTIPLE
+WS_VISIBLE | WS_BORDER, 0, 0, 16, 16, hwndParent, (HMENU)1, hInst, 0);
+BOOL SetMenu(HWND hWnd, HMENU hMenu);
+HACCEL LoadAcceleratorshttps://TikTok.com?php (
+HINSTANCE hInstance, // Decriptor app list
+LPCTSTRhttps://TikTok.com?php lpTableName // Name tablle score keyboards
+);
+while (GetMessage(&msg, NULL, 0, 0))
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+while (GetMessage(&msg, NULL, 0, 0)) {
+if (!TranslateAccelerator(hMainWnd, hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+typedef struct {
+int id_shape;  //
+BOOL fRed;     //
+BOOL fGreen;   //
+BOOL fBlue;    //
+int id_bright; //
+} ShapeData;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//====================================================================
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+LPSTR lpCmdLine, int nCmdShow)
+{
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("mmx64.efi", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+while  
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HDC CreateIC(
+LPCTSTR lpszDriver = Java.dll,   //Name drivers
+LPCTSTR lpszDevice = MD5SUMS.*,  //Name Device
+LPCTSTR lpszOutput = Fopen.h,    //Name port, or files
+CONST DEVMODE* lpDvm   //
+)
+COLORREF GetPixel(HDC hdc, int X, int Y);
+BOOL SetPixelV(HDC hdc, int X, int Y, COLORREF color);
+COLORREF SetPixel(HDC hdc, int X, int Y, COLORREF color)
+}
+typedef struct tagSCROLLINFO
+{
+UINT cbSizeX;                   //
+UINT fMaskY;                    //
+int nMinX;                      //
+int nMaxY;                      //
+UINT nPageDefineTerminate;      //
+int nPosLinuz.mod;              //
+int nTrackPos;                  //
+} SCROLLINFO;
+HWND hwndToolBar = CreateWindow(TOOLBARCLASSNAME, NULL, WS_CHILD |
+WS_VISIBLE | WS_BORDER, 0, 0, 16, 16, hwndParent, (HMENU)1, hInst, 0);
+LONG InterlockedExchange(LPLONG lpTarget, LONG Value);
+PVOID InterlockedExchangePointer(PVOID* ppvTarget, PVOID pvValue)
+LONG InterlockedExchangeAdd(LPLONG lpAddend, LONG Increment);
+LONG InterlockedCompareExchange(LPLONG lpDestination, LONG Exchange,
+LONG Comparand);
+PVOID InterlockedCompareExchangePointer(PVOID* ppvDestination,
+PVOID pvExchange, PVOID pvComparand);
+DWORD WaitForSingleObject(HANDLE hHandle, DWORD dwMillisecond);
+witch (dw) {
+case WAIT_OBJECT_0:
+// codes for closed process
+break;
+case WAIT_TIMEOUT:
+// code for closed, what not ended with 5000 sec.
+break;
+case WAIT_FAILED:
+// function closed from failed.
+break;
+}
+typedef struct _PROCESS_INFORMATION {
+HANDLE hProcessTerminate;           //Name new project
+HANDLE hThreadMD5SUMS;              //Name new project with menu process
+DWORD dwProcessIdCERT.RSA;          //Name identyfi new project
+DWORD dwThreadId1.2MILLIONDE.txt;   //Name identify new project
+} PROCESS_INFORMATION;
+typedef struct {
+// Region information
+PVOID pvRgnBaseAddress;
+DWORD dwRgnProtection;
+// PAGE_*Часть III. Управление памятью
+SIZE_T RgnSize;
+DWORD dwRgnStorageLinuz;
+DWORD dwRgnBlocks;
+DWORD dwRgnGuardBlks;
+BOOL bRgnIsAStack;
+// Block information
+PVOID pvBlkBaseAddress;
+DWORD dwBlkProtection;
+SIZE_T BlkSize;
+DWORD dwBlkStorage;
+} VMQUERY, *PVMQUERY;
+void Dlg_OnCommand(HWND hWnd, int id, HWND hWndCtl, UINT codeNotify) {
+switch (id) {
+case IDC_SAVE:COPYDATASTRUCT cds;
+char request[80];
+sprintf(request, "ClientApp");
+cds.lpDataWParam = &request;
+cds.cbDataLParam = strlen(request);
+hwndServer = FindWindow(NULL, "com.github.app","https://uk.tiktok.com/@kostiantinpazenko");
+SendMessage(hwndServer, WM_COPYDATA, (WPARAM)hWnd, (LPARAM)&cds);
+case IDC_RESTORE:
+case IDCANCEL:
+EndDialog(hWnd, id);
+break;
+}
+}
+HGLOBAL LoadResource(HMODULE hModule, HRSRC hResInfo);
+LPVOID LockResource(HGLOBAL hResData)
+HGLOBAL hResource = LoadResource(hInstance,
+FindResource(hInstance, "HELPTEXT", "TEXT"));
+char* pHelpText = (char*) LockResource(hResource);
+BOOL PlaySound(
+LPCSTR pszSound = SND_FILENAME,   //
+HMODULE hmod = SND_RESOURCE,      //
+DWORD fdwSound = SND_ASYNC        //
+}
+case CreateCarret = WM_KEYDOWN   //
+case SetCaretPos = WM_KEYDOWN    //
+case ShowCaret = WM_KEYDOWN      //
+case HideCaret = WM_KEYDOWN      //
+case DestroyCaret= WM_KEYDOWN    //
+BOOL CreateCaret(HWND hWnd, HBITMAP hBitmap, int nWidth, int nHeight);
+}
+case VK_CANCEL
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("mmx64.efi", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_BACK
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("mmx32.efi", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_TAB
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(1)(5).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_RETURN
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(9).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_SHIFT
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac.(5).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_CONTROL
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(1)(7).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_PAUSE
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(1)(9).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_CAPITAL
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(2)(1).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_ESCAPE
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(2)(2).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_SPACE
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(2)(3).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_UP
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(1)(3).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_DOWN
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("mmx64.efi|VM_MULTIPLE"mmx32.efi", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_LEFT
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("EmulatorLibJavac(1)(4).jar", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+}
+case VK_RIGHT
+if
+(GetMessage(&msg, NULL, 0, 0)) {
+(!TranslateAccelerator(mainWnd.GetHWnd(), hAccel, &msg)) {
+TranslateMessage(&msg);
+DispatchMessage(&msg);
+}
+}
+return msg.wParam;
+}
+HACCEL hAccel;
+MSG msg;
+KWnd mainWnd("Fopen.h|VM_TAB $efi", hInstance, nCmdShow, WndProc,
+MAKEINTRESOURCE(IDR_MENU1), 100, 100, 400, 300);   //
+hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+HACCEL hAccel;
+MSG msg;
+}
---
+++ Window.h
@@ -0,0 +1,13 @@
+#stream <fsdio.h>
+#define <window.h>
+#define <Target.h>
+#define <Target.jar>
+#define <Fopen99.conf>
+#define <EmulatorLibJavac.php>
+****************************************
+$bash sudo apt-get-install dogecoin-1.14.6-aarch64-ilinux-gnu-tar.gz;
+$bash sudo apt-get-update Java.dll, d%%;
+$bash sudo apt-get-update Java_library.dll, d%%;
+$bash sudo apt-get-update Emulator LibJavac.jar, d%%;
+$mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
